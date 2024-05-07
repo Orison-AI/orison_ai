828850444f2d20c62b8f40c93bd5fdc01bb62274
Andrew Lucas <andrew.tyler.lucas@gmail.com>
2024-05-07 07:53:15 -0400

    Menu buttons

--- frontend/orison-ai-frontend/src/pages/MainMenu.jsx
+++ frontend/orison-ai-frontend/src/pages/MainMenu.jsx
@@ -2,24 +2,37 @@
 
 import React from 'react';
 import {
-  Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton,
-  DrawerHeader, DrawerBody,
-  Link, VStack,
+  Button,
+  Drawer, DrawerOverlay, DrawerContent,
+  DrawerCloseButton, DrawerHeader, DrawerBody,
+  VStack,
 } from '@chakra-ui/react';
 
 function MainMenu({ isOpen, onClose }) {
+
+  const MenuButton = ({label}) => (
+    <Button 
+      variant="ghost"
+      width="100%"
+      justifyContent="flex-start"
+      _hover={{ bg: "gray.600" }}
+    >
+      {label}
+    </Button>
+  );
+
   return (
     <Drawer isOpen={isOpen} onClose={onClose} placement="left">
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton />
         <DrawerHeader>orison.ai</DrawerHeader>
-        <DrawerBody>
+        <DrawerBody pl="4vh">
           <VStack spacing={4}>
-            <Link href="#">Select Applicant</Link>
-            <Link href="#">Upload Documents</Link>
-            <Link href="#">Initial Evaluation</Link>
-            <Link href="#">Detailed Evaluation</Link>
+            <MenuButton label="Select Applicant" />
+            <MenuButton label="Upload Documents" />
+            <MenuButton label="Initial Evaluation" />
+            <MenuButton label="Detailed Evaluation" />
           </VStack>
         </DrawerBody>
       </DrawerContent>
