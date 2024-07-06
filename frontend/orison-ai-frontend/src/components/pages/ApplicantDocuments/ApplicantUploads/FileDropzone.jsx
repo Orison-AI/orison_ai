a392348dab0d5d9c48dfccddd485df7d2ea7e3bd
Andrew Lucas <ATLucas@users.noreply.github.com>
2024-07-06 14:19:42 -0400

    [65] uploading files to frontend shows no feedback so we dont know if file is uploading or not (#77)
    * Disabling dropzone if file upload is in progress
    * Showing upload modal while uploading a file
    * Adding file upload spinner

--- frontend/orison-ai-frontend/src/components/pages/ApplicantDocuments/ApplicantUploads/FileDropzone.jsx
+++ frontend/orison-ai-frontend/src/components/pages/ApplicantDocuments/ApplicantUploads/FileDropzone.jsx
@@ -7,11 +7,14 @@ import React from 'react';
 import { useDropzone } from 'react-dropzone';
 
 // Chakra UI
-import { VStack, Icon, Text, Link } from '@chakra-ui/react';
+import { VStack, Icon, Link, Spinner, Text } from '@chakra-ui/react';
 import { DownloadIcon } from '@chakra-ui/icons';
 
-const FileDropzone = ({ onDrop }) => {
-  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({ onDrop });
+const FileDropzone = ({ onDrop, disabled }) => {
+  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+    onDrop: disabled ? () => {} : onDrop,
+    disabled,
+  });
 
   return (
     <VStack
@@ -21,12 +24,19 @@ const FileDropzone = ({ onDrop }) => {
       p="20px"
       mb="20px"
       backgroundColor={isDragActive ? 'gray.700' : 'transparent'}
+      cursor={disabled ? 'not-allowed' : 'pointer'} // Change cursor when disabled
     >
-      <input {...getInputProps()} />
-      <Icon as={DownloadIcon} color="gray.500" />
-      <Text fontSize="20px">
-        <Link as="b" onClick={open} cursor="pointer">Choose a file</Link> or drag it here
-      </Text>
+      <input {...getInputProps()} disabled={disabled} />
+      {disabled ? (
+        <Spinner size="xl" />
+      ) : (
+        <>
+          <Icon as={DownloadIcon} color="gray.500" />
+          <Text fontSize="20px">
+            <Link as="b" onClick={open} cursor="pointer">Choose a file</Link> or drag it here
+          </Text>
+        </>
+      )}
     </VStack>
   );
 };
